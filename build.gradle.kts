fa12d026b9aa7675a23b3791fa2886218c4fdf23
subhra-io <subhrajeetpandey2001@gmail.com>
2026-04-22 13:08:38 +0530

    Fix JitPack build - remove maven-publish from root gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("com.android.library") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    id("maven-publish") apply false
 }
 
 tasks.register("clean", Delete::class) {
